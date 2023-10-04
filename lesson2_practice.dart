87e6aa7ff6b5ac91f28a78536e0a755d33e73e9c
Dastanbek <muktaraliuuludastanbek@gmail.com>
2023-10-05 01:27:50 +0600

    first commit

---
+++ lesson2_practice.dart
@@ -0,0 +1,53 @@
+void main(List<String> args) {
+  num n1 = 2,
+      n2 = 3.4,
+      n3 = 10.0,
+      n4 = 2.2,
+      n5 = 0; //5 переменных со значениями типа number.
+
+  int i1 = 1,
+      i2 = 10,
+      i3 = 100,
+      i4 = -43,
+      i5 = 0; //5 переменных со значениями типа integer.
+  double d1 = 0,
+      d2 = 2.2,
+      d3 = 3,
+      d4 = -4.3,
+      d5 = 100; //5 переменных со значениями типа double.
+  String str1 = "Dastan",
+      str2 = 'Muktarali',
+      str3 = 'uulu',
+      str4 = 'flutter',
+      str5 = 'dart'; //5 переменных со значениями типа String.
+  List array1 = [
+    5,
+    2,
+    3.0,
+    'hello world',
+    true,
+    false,
+    'dartpad'
+  ]; //массив с разными типами значений элементов.
+  List<int> array2 = [
+    2,
+    3,
+    4,
+  ]; //массив с определенным типом значений элементов
+  List<double> array3 = [
+    3,
+    0,
+    2.3,
+    3.4,
+    -5,
+  ];
+  List<String> array4 = [
+    'python',
+    'java',
+    'android',
+  ];
+  List<bool> array5 = [
+    true,
+    false,
+  ];
+}
